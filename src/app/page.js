2ed0a8689a3ee210a8626e41fbd928824d0604c5
Alexis Tercero <alexistercero55@gmail.com>
2023-03-24 23:09:11 -0600

    TASK | Updating the state at context by useState pattern.
    Signed-off-by: Alexis Tercero <alexistercero55@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client"
+import { useState } from 'react';
 import { useTask } from '@/context/taskContext';
 import { TaskCard } from '@/components/TeaskCard';
 import { Inter } from 'next/font/google';
@@ -7,11 +8,40 @@ import styles from './page.module.css';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const tasks = useTask();
+  const [task, settask] = useState();
+  const {tasks,newTask} = useTask();
+
+  /** The square brackets around 'e.target.name' are necessary because the property name is determined dynamically at runtime based on the name attribute of the input field that was changed. The square brackets tell JavaScript to evaluate the expression inside the brackets and use its value as the property name. */
+  const handleChange = (e)=>{
+    settask({...task, [e.target.name]: e.target.value});
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    newTask(task.title, task.description)
+  }
+
   return (
     <main className={styles.main}>
 
+      <section>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input type="text" 
+          name='title'
+          onChange={handleChange} />
+        </label>
+        <label>
+          Description:
+          <textarea name={'description'} onChange={handleChange} />
+        </label>
+        <button type="submit">Add Task</button>
+      </form>
+      </section>
+
       <div>
+        <button>new task</button>
         
         <div className="task-list">
           {tasks.map(task => (
--- src/context/taskContext.js
+++ src/context/taskContext.js
@@ -1,5 +1,6 @@
 "use client"
-import {createContext, useContext} from 'react';
+import {createContext, useContext, useState} from 'react';
+import { v4 as uuid } from "uuid";
 
 export const TaskContext = createContext();
 // custom hook
@@ -10,11 +11,20 @@ export const useTask = ()=>{
     return data;
 }
 
+const initialState = [{    "id": 1,    "title": "Complete project proposal",    "description": "Write a proposal for the new project and submit it by the deadline."  },  {    "id": 2,    "title": "Meet with client",    "description": "Schedule a meeting with the client to discuss the project requirements and scope."  },  {    "id": 3,    "title": "Design wireframes",    "description": "Create wireframes for the user interface and get approval from the client."  },  {    "id": 4,    "title": "Develop frontend",    "description": "Develop the frontend of the application using ReactJS and styled components."  },  {    "id": 5,    "title": "Develop backend",    "description": "Develop the backend of the application using NodeJS and MongoDB."  },  {    "id": 6,    "title": "Write tests",    "description": "Write automated tests for both frontend and backend to ensure quality and prevent regressions."  },  {    "id": 7,    "title": "Deploy to staging",    "description": "Deploy the application to the staging environment for testing and feedback."  },  {    "id": 8,    "title": "Fix bugs and issues",    "description": "Fix any bugs and issues found during testing and make necessary improvements."  },  {    "id": 9,    "title": "Deploy to production",    "description": "Deploy the application to the production environment after all issues have been resolved."  },  {    "id": 10,    "title": "Document the project",    "description": "Create documentation for the project including the codebase, API endpoints, and deployment instructions."  }];
+
 //provider
-export const TaskProvider = ({children, ...props}) => {
-    const tasks = [  {    "id": 1,    "title": "Complete project proposal",    "description": "Write a proposal for the new project and submit it by the deadline."  },  {    "id": 2,    "title": "Meet with client",    "description": "Schedule a meeting with the client to discuss the project requirements and scope."  },  {    "id": 3,    "title": "Design wireframes",    "description": "Create wireframes for the user interface and get approval from the client."  },  {    "id": 4,    "title": "Develop frontend",    "description": "Develop the frontend of the application using ReactJS and styled components."  },  {    "id": 5,    "title": "Develop backend",    "description": "Develop the backend of the application using NodeJS and MongoDB."  },  {    "id": 6,    "title": "Write tests",    "description": "Write automated tests for both frontend and backend to ensure quality and prevent regressions."  },  {    "id": 7,    "title": "Deploy to staging",    "description": "Deploy the application to the staging environment for testing and feedback."  },  {    "id": 8,    "title": "Fix bugs and issues",    "description": "Fix any bugs and issues found during testing and make necessary improvements."  },  {    "id": 9,    "title": "Deploy to production",    "description": "Deploy the application to the production environment after all issues have been resolved."  },  {    "id": 10,    "title": "Document the project",    "description": "Create documentation for the project including the codebase, API endpoints, and deployment instructions."  }];
+export const TaskProvider = ({children}) => {
+    const [tasks, setTasks] = useState(initialState);
+    const newTask = (title,description) =>{
+        setTasks([...tasks, {
+            id: uuid(),
+            title,
+            description,
+        }])
+    }
     return(
-        <TaskContext.Provider value={tasks}>
+        <TaskContext.Provider value={{tasks, newTask}}>
             {children}
         </TaskContext.Provider>
     );
